a45c3ab16a16b609311d9f6c7933db388d2c2ae2
rmit-s3614081-suyash-kalyankar <s3614081@student.rmit.edu.au>
2017-03-23 21:04:32 +1100

    Delete Athlete.java

--- Athlete.java
+++
@@ -1,9 +0,0 @@
-package ass1;
-
-public class Athlete extends Person {
-
-	public Athlete(String id, String name, int age, int state) {
-		super(id, name, age, state);
-		// TODO Auto-generated constructor stub
-	}
-}
